133a8055d411f53c616e9a0aa37fe1d44ff06649
olyamerzlyakova <olya.mr7@gmail.com>
2023-11-19 00:51:22 +0500

    первый вид товара

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+  const productTitle = 'Coffee grinder';
+  const productCategory = 'Kitchen appliances';
+  const productQuantity = 7;
+  const productPrice = 1000;
+
+  console.log(`Название товара: ${productTitle}`);
+  console.log(`Общая сумма товара: ${productQuantity * productPrice}`);
+}
+
+
+
