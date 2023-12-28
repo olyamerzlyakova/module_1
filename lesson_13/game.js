f0069d66965900adc620ae9c00fe0ccaa337c73c
olyamerzlyakova <olya.mr7@gmail.com>
2023-12-28 08:01:00 +0500

    правка

--- lesson_13/game.js
+++ lesson_13/game.js
@@ -1,9 +1,6 @@
 'use strict';
 
 (() => {
-  const FIGURES_RUS = ['ножницы', 'камень', 'бумага'];
-  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
-
   const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -15,21 +12,44 @@
     return lang[randomInt];
   };
 
+  const RULang = {
+    figures: ['ножницы', 'камень', 'бумага'],
+    exitConfirmation: 'Вы точно хотите выйти?',
+    computer: 'Компьютер',
+    you: 'Вы',
+    computersChoice: 'Компьютер выбрал',
+    yourChoice: 'Вы выбрали',
+    theDraw: 'Ничья',
+    youWin: 'Вы выиграли!',
+    computerWins: 'Компьютер выигрывает :(',
+  };
+  const ENLang = {
+    figures: ['scissors', 'rock', 'paper'],
+    exitConfirmation: 'Are you sure you want to come out?',
+    computer: 'Computer',
+    you: 'You',
+    computersChoice: 'Computer has chosen',
+    yourChoice: 'You have chosen',
+    theDraw: 'The draw',
+    youWin: 'You win!',
+    computerWins: 'Computer wins :(',
+  };
+
   const game = (language) => {
     const result = {
       player: 0,
       computer: 0,
     };
-    const lang =
-      language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_RUS;
+
+    const lang = language === 'EN' || language === 'ENG' ? ENLang : RULang;
 
     return function start() {
-      const computerFigure = getComputerFigure(lang);
+      const computerFigure = getComputerFigure(lang.figures);
       console.log('computerFigure: ', computerFigure);
-      const gamerFigure =
-        lang === FIGURES_ENG
-          ? prompt('Rock, scissors, paper?', 'paper')
-          : prompt('Камень, ножницы, бумага?', 'камень');
+      const gamerFigure = prompt(
+        `${lang.figures[0]}, ${lang.figures[1]}, ${lang.figures[2]}`,
+        `${lang.figures[0]}`
+      );
       console.log('gamerFigure: ', gamerFigure);
 
       const checkGamerFigure = () => {
@@ -38,96 +58,56 @@
           gamerFigure !== '' &&
           gamerFigure !== undefined
         ) {
-          return lang.some((item) => item.includes(gamerFigure.toLowerCase()));
+          return lang.figures.some((item) =>
+            item.includes(gamerFigure.toLowerCase())
+          );
         } else {
           return false;
         }
       };
 
-      if (lang === FIGURES_RUS) {
-        if (gamerFigure === null) {
-          const gamerDecision = confirm('Вы точно хотите выйти?');
-          if (gamerDecision) {
-            return alert(`Компьютер: ${result.computer}
-                          Вы: ${result.player}`);
-          } else {
-            return start();
-          }
-        }
-        if (gamerFigure === '' || checkGamerFigure() === false) {
+      if (gamerFigure === null) {
+        const gamerDecision = confirm(lang.exitConfirmation);
+        if (gamerDecision) {
+          return alert(`${lang.computer}: ${result.computer}
+          ${lang.you}: ${result.player}`);
+        } else {
           return start();
-        }
-        if (checkGamerFigure() === true) {
-          const checkedGamerFigure = lang.find((item) =>
-            item.includes(gamerFigure.toLowerCase())
-          );
-          console.log('checkedGamerFigure: ', checkedGamerFigure);
-          if (checkedGamerFigure === computerFigure) {
-            alert(`Компьютер выбрал: ${computerFigure}
-             Вы выбрали: ${checkedGamerFigure} 
-             Ничья`);
-            return start();
-          }
-          if (
-            (computerFigure === 'камень' && checkedGamerFigure === 'бумага') ||
-            (computerFigure === 'ножницы' && checkedGamerFigure === 'камень') ||
-            (computerFigure === 'бумага' && checkedGamerFigure === 'ножницы')
-          ) {
-            result.player++;
-            alert(`Компьютер выбрал: ${computerFigure}
-          Вы выбрали: ${checkedGamerFigure}
-          Вы выиграли!`);
-            return start();
-          } else {
-            result.computer++;
-            alert(`Компьютер выбрал: ${computerFigure}
-                   Вы выбрали: ${checkedGamerFigure}
-                   Компьютер выиграл :(`);
-            return start();
-          }
         }
       }
-      if (lang === FIGURES_ENG) {
-        if (gamerFigure === null) {
-          const gamerDecision = confirm('Are you sure you want to come out?');
-          if (gamerDecision) {
-            return alert(`Computer: ${result.computer}
-          You: ${result.player}`);
-          } else {
-            return start();
-          }
-        }
-        if (gamerFigure === '' || checkGamerFigure() === false) {
+      if (gamerFigure === '' || checkGamerFigure() === false) {
+        return start();
+      }
+      if (checkGamerFigure() === true) {
+        const checkedGamerFigure = lang.figures.find((item) =>
+          item.includes(gamerFigure.toLowerCase())
+        );
+        console.log('checkedGamerFigure: ', checkedGamerFigure);
+        if (checkedGamerFigure === computerFigure) {
+          alert(`${lang.computersChoice}: ${computerFigure}
+           ${lang.yourChoice}: ${checkedGamerFigure} 
+           ${lang.theDraw}`);
           return start();
         }
-        if (checkGamerFigure() === true) {
-          const checkedGamerFigure = lang.find((item) =>
-            item.includes(gamerFigure.toLowerCase())
-          );
-          console.log('checkedGamerFigure: ', checkedGamerFigure);
-          if (checkedGamerFigure === computerFigure) {
-            alert(`Computer has chosen: ${computerFigure}
-                   You have chosen: ${checkedGamerFigure}
-                   The draw`);
-            return start();
-          }
-          if (
-            (computerFigure === 'rock' && checkedGamerFigure === 'paper') ||
-            (computerFigure === 'scissors' && checkedGamerFigure === 'rock') ||
-            (computerFigure === 'paper' && checkedGamerFigure === 'scissors')
-          ) {
-            result.player++;
-            alert(`Computer has chosen: ${computerFigure}
-             You have chosen: ${checkedGamerFigure}
-             You win!`);
-            return start();
-          } else {
-            result.computer++;
-            alert(`Computer has chosen: ${computerFigure}
-                   You have chosen: ${checkedGamerFigure}
-                   Computer wins :(`);
-            return start();
-          }
+        if (
+          (computerFigure === lang.figures[1] &&
+            checkedGamerFigure === lang.figures[2]) ||
+          (computerFigure === lang.figures[0] &&
+            checkedGamerFigure === lang.figures[1]) ||
+          (computerFigure === lang.figures[2] &&
+            checkedGamerFigure === lang.figures[0])
+        ) {
+          result.player++;
+          alert(`${lang.computersChoice}: ${computerFigure}
+        ${lang.yourChoice}: ${checkedGamerFigure}
+        ${lang.youWin}`);
+          return start();
+        } else {
+          result.computer++;
+          alert(`${lang.computersChoice}: ${computerFigure}
+                 ${lang.yourChoice}: ${checkedGamerFigure}
+                 ${lang.computerWins}`);
+          return start();
         }
       }
     };
